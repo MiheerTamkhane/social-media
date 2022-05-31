4fd220708ec8e151c004d6e0280189342fbc6cb0
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-05-31 14:29:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
