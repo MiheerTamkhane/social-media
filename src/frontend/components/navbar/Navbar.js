a7270ca84db5333cd8502ac0823920905dd15d3e
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-06-05 10:32:38 +0530

    update suggested by peers

--- src/frontend/components/navbar/Navbar.js
+++ src/frontend/components/navbar/Navbar.js
@@ -7,11 +7,6 @@ export const Navbar = () => {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-purple-700 font-['rajdhani']">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <NavLink to="/" className="flex items-center">
-          {/* <img
-            src="/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          /> */}
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white tracking-wider">
             OtakuSurf
           </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,16 @@ module.exports = {
       },
     },
     screens: {
+      // => @media (min-width: ...) { ... }
       sm: { min: "640px" },
       md: { min: "768px" },
       lg: { min: "1024px" },
       xl: { min: "1280px" },
       minmd: { min: "968px" },
+      // => @media (max-width: ...) { ... }
       maxmidmd: { max: "768px" },
       maxmd: { max: "968px" },
       maxxlmd: { max: "1280px" },
-      // => @media (max-width: 767px) { ... }
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
